abbbefcc755f395dcb13afa50c5b149fcf0c9567
Pedro Belo <pedrosmmbelo@gmail.com>
2023-12-31 01:27:33 +0000

    second problem of day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::max;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead, Result};
 use std::path::Path;
@@ -20,44 +22,30 @@ where P: AsRef<Path>, {
 }
 
 fn process_line(line: &String) -> u32 {
+	let mut map: HashMap<&str, u32> = HashMap::new();
+
 	let single_line = line.split(": ")
 		.collect::<Vec<_>>();
 
-	let line_number: u32 = single_line[0]
-		.split(" ")
-		.collect::<Vec<_>>()[1]
-		.parse()
-		.unwrap();
-
-	let games = single_line[1]
+	let binding = single_line[1]
+		.replace(",", ";");
+ 	let colors = binding
 		.split("; ")
-		.map(|game| game.to_string())
 		.collect::<Vec<_>>();
 
-	let are_all_games_valid: bool = games.iter()
-		.map(|color_and_value| is_set_valid(color_and_value))
-		.fold(true, |acc, x| acc && x);
-	
-	if are_all_games_valid {
-		return line_number;
-	}
-	return 0;
-}
+	colors.iter()
+		.for_each(|color| {
+			let color_and_value = color.split(" ").collect::<Vec<_>>();
+			let color = color_and_value[1];
+			let value: u32 = color_and_value[0].parse().unwrap();
+			map.entry(color)
+				.and_modify(|in_map| { *in_map =  max::<u32>(*in_map, value); })
+				.or_insert(value);
+		});
 
-fn is_set_valid(set: &String) -> bool {
-	set.split(", ")
-		.map(|color_and_number| is_color_valid(color_and_number.to_string()))
-		.fold(true, |acc, x| acc && x)
-}
+	let mut result = 1;
+	map.iter()
+		.for_each(|(_, value)| result *= value);
 
-fn is_color_valid(color: String) -> bool {
-	let color_and_value = color.split(" ").collect::<Vec<_>>();
-	let value: u32 = color_and_value[0].parse().unwrap();
-	let color = color_and_value[1];
-	
-	if color.eq("red") && value > 12 { return false };
-	if color.eq("green") && value > 13 { return false };
-	if color.eq("blue") && value > 14 { return false };
-
-	true
+	return result;
 }
